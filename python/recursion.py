3348537cb963fb0bc01ddab2a11e0669b89a9069
nemo0702 <abhisingh7294@gmail.com>
2019-11-09 16:37:24 +0530

    Python recursion

---
+++ python/recursion.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+if __name__ == '__main__':
+    n = int(input())
+    result = factorial(n)
+    print(result)
